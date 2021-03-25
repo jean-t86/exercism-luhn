22fd97dcca203b362661b45e0207b574e85cc7d6
J.Tadebois <jpm.tadebois@gmail.com>
2021-03-25 15:44:22 +0400

    Add completed solution

--- src/main/kotlin/Luhn.kt
+++ src/main/kotlin/Luhn.kt
@@ -1,6 +1,37 @@
 object Luhn {
 
     fun isValid(candidate: String): Boolean {
-        TODO("Implement this function to complete the task")
+        // Remove all spaces in candidate string
+        val noSpaces = candidate.replace(" ", "")
+
+        if (noSpaces.length <= 1) {
+            return false
+        }
+
+        // Check that only numbers are left in candidate string
+        // If not return false
+        if (!noSpaces.all { char ->
+                char.toInt() in '0'.toInt()..'9'.toInt()
+            }) {
+            return false
+        }
+
+        val sum= noSpaces
+            .reversed()
+            .map { it.toString().toInt() }
+            .mapIndexed { index, num ->
+                if (index % 2 != 0) {
+                    val doubled = num * 2
+                    if (doubled > 9) {
+                        doubled - 9
+                    } else {
+                        doubled
+                    }
+                } else {
+                    num
+                }
+            }.sum()
+
+        return sum % 10 == 0
     }
 }
